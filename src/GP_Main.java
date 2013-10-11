ef8813ea68879ae5f7d2279a10caffd3e64728ad
warsamebashir <warz07@yahoo.com>
2013-10-10 20:21:15 -0500

    Initial Project Strucutre - GP Main and Settings file

---
+++ src/GP_Main.java
@@ -0,0 +1,18 @@
+/**
+ * Main GP 
+ * @author bash1664
+ *
+ */
+
+public class GP_Main {
+
+	/**
+	 * Main Run() 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
---
+++ src/Settings.java
@@ -0,0 +1,10 @@
+/**
+ * Displays settings of the GP program including but not limited to {Probabilities ..}
+ * @author bash1664
+ *
+ */
+public class Settings {
+	
+	private double MUTATOR_PROB = 0.5;
+
+}
